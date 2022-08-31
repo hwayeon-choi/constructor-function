f1210f92d10d0e34eb6fd3a0c1b1c50e03f68216
Hwayeon Choi <hwayeon.choi11@gmail.com>
2022-08-31 11:10:18 +0900

    instance

---
+++ instance.js
@@ -0,0 +1,12 @@
+export default function numbers(one, two, three) {
+  if(typeof(one) === "number" 
+    && typeof(two) === "number" 
+    && typeof(three) === "number") {
+    this.first = one;
+    this.second = two;
+    this.third = three;
+  }
+}
+
+const objectData = new numbers(10,12,13);
+console.log(objectData);
---
+++ kdt.js
@@ -0,0 +1,15 @@
+export default function kdt(name, glass, payment) {
+  this.id = name;
+  this.b = glass;
+  this.c = payment;
+}
+
+// const members = [];
+// for(let i = 0; i < 5; i++) {
+//   members.push(new kdt(i + 1, "hello", "bye"));
+// }
+
+// console.log(members);
+
+// const  = new kdt("슈크림", "김치", "소주");
+// console.log(typeof(ahyeon));
